66b5ec3685b8c6fd70d3fb210dd432297ab9902c
justbegginer <46086383+justbegginer@users.noreply.github.com>
2020-12-13 12:13:06 +0300

    make Array class

---
+++ Array.cpp
@@ -0,0 +1,5 @@
+//
+// Created by user.v2.0 on 13.12.2020.
+//
+
+#include "Array.h"
---
+++ Array.h
@@ -0,0 +1,14 @@
+//
+// Created by user.v2.0 on 13.12.2020.
+//
+
+#ifndef PAYMENT_ARRAY_H
+#define PAYMENT_ARRAY_H
+
+
+class Array {
+
+};
+
+
+#endif //PAYMENT_ARRAY_H
