fda26840febf407d584c4dcf43530bd83821d0c5
justbegginer <46086383+justbegginer@users.noreply.github.com>
2020-12-14 12:04:46 +0300

    format code , add ,make empty array = nulptr , add index checker , fix push_buck func

--- Array.h
+++ Array.h
@@ -5,23 +5,34 @@
 #ifndef PAYMENT_ARRAY_H
 #define PAYMENT_ARRAY_H
 
+#include <ostream>
+
 template<typename T>
 class Array {
 private:
     int length;
     T *array;
+
     void make_bigger();
+
 public:
     Array();
-    Array(T *array , int length);
+
+    Array(T *array, int length);
+
     ~Array();
-    void push_back(T new_element);
+
+    void push_back(T &new_element);
+
     int get_length();
-    T& operator[](int index);
+
+    T &operator[](int index);
+   // friend std::ostream &operator<<(std::ostream &os, const Array<T> &array);
 };
+
 template<typename T>
 Array<T>::Array() {
-    array =new T[0];
+    array = nullptr;
     length = 0;
 }
 
@@ -33,13 +44,13 @@ Array<T>::Array(T *array, int length) {
 
 template<typename T>
 Array<T>::~Array() {
-    delete [] array;
+    delete[] array;
 }
 
 template<typename T>
-void Array<T>::push_back(T new_element) {
+void Array<T>::push_back(T &new_element) {
     make_bigger();
-    array[length - 1] = new_element;
+    *(array+length-1) = new_element;
 }
 
 template<typename T>
@@ -49,21 +60,26 @@ int Array<T>::get_length() {
 
 template<typename T>
 T &Array<T>::operator[](int index) {
-    return *(array+index);
+    if (index < 0 || index >= length)
+        throw std::out_of_range("index in Array out of range\n");
+    return *(array + index);
 }
 
 template<typename T>
 void Array<T>::make_bigger() {
-    T *new_array = new T[length];
+    T *new_array = new T[length + 1];
     for (int i = 0; i < length; ++i) {
-        *(new_array + i) = *(array+i);
+        *(new_array + i) = *(array + i);
     }
-    array = new T[length+1];
-    for (int i = 0; i < length; ++i) {
-        *(array+i) = *(new_array+i);
-    }
-    delete [] new_array;
     length++;
+    array = new_array;
 }
+
+
+//template<typename T>
+//std::ostream &operator<<(std::ostream &os, const Array<T> &array) {
+//    os << "length: " << array.length << " array: " << array.array;
+//    return os;
+//}
 
 #endif //PAYMENT_ARRAY_H
