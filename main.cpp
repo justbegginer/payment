f10ac14e0822839dea90d86d7bcdf523105e6082
justbegginer <46086383+justbegginer@users.noreply.github.com>
2020-12-15 12:45:45 +0300

    make table-maker

--- main.cpp
+++ main.cpp
@@ -71,3 +71,71 @@ void build_array(Array<Payment> &payments, std::fstream &file) {
     }
 
 }
+
+
+void tab(Array<Payment> &array) {
+    std::vector<std::string> payers;
+    std::vector<std::string> payee;
+    for (int i = 0; i < array.get_length(); ++i) {
+        bool new_payer = true;
+        std::string temp = array[i].getPayerOrg();
+        for (int j = 0; j < payers.size(); ++j) {
+            if (payers[j] == temp) {
+                payee[j] += ", " + array[i].getPayeeOrg();
+                new_payer = false;
+                break;
+            }
+        }
+        if (new_payer) {
+            payers.push_back(temp);
+            payee.push_back(array[i].getPayeeOrg());
+        }
+    }
+    int first = 18, second = 18;
+    for (int i = 0; i < payers.size(); i++) {
+        if (payers[i].size() > first) {
+            first = payers[i].size();
+        }
+        if (payee[i].size() > second) {
+            second = payee[i].size();
+        }
+    }
+    std::ofstream table;
+    try {
+        table.open(R"(C:\Users\user.v2.0\CLionProjects\payment\table)");
+        std::string upline = "";
+        for (int i = 0; i < first + second + 7; ++i) {
+            upline += "-";
+        }
+        table << upline << "\n";
+        std::string temp = "| payer organisation";
+        for (int j = 0; j < first - 18; ++j) {
+            temp += " ";
+        }
+        temp += "| payee organisation";
+        for (int j = 0; j < second - 18 + 2; ++j) {
+            temp += " ";
+        }
+        temp += "|";
+        table << temp << "\n";
+        for (int i = 0; i < payers.size(); ++i) {
+            table << upline << "\n";
+            temp = "| " + payers[i];
+            for (int j = 0; j < first - payers[i].size(); ++j) {
+                temp += " ";
+            }
+            temp += "| " + payee[i];
+            for (int j = 0; j < second - payee[i].size() + 2; ++j) {
+                temp += " ";
+            }
+            temp += "|";
+            table << temp << "\n";
+        }
+        table << upline << "\n";
+        table.close();
+    }
+    catch (...) {
+        table.close();
+        throw std::invalid_argument("writing file error");
+    }
+}
